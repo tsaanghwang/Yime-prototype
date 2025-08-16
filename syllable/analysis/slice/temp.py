d45b95b27b1b3a94c167d0d911754403f45eb82d
yinyuanxitong <yinyuanxitong@foxmail.com>
2025-08-16 13:56:23 +0800

    Update

--- syllable/analysis/slice/temp.py
+++ syllable/analysis/slice/temp.py
@@ -1,7 +1,8 @@
 """
-在syllable\analysis\slice\ganyin_to_yinyuan_sequence.py中：
-1. 保留现有功能不变
-2. 对经过当前模块处理还未写入文件的中间数据，调用syllable\analysis\slice\yueyin_yinyuan.py中定义的_change_pitch_style方法
-改变音元的音调标记方式, 把结果写入ganyin_to_yinyuan_seq_marks.json文件中
-3. 同时检查syllable\analysis\slice\yueyin_yinyuan.py中是处理音调标记的逻辑是否完整和正确
-"""
+修改84-85行：
+    for final in triple_quality:
+        print(f"   '{final}' - 长度: {len(final.replace('ng', 'N').replace('io', 'y'))}")
+改成：
+    当韵母长度==2 and 第二个单位（字符）是"i"/"n"时，输出信息："脱落韵腹[ᵊ]的三质韵母"
+    当韵母长度==2 and 第二个单位（字符或字符组合）是"u"/"ng"时，输出信息："脱落韵腹[𐞑]的三质韵母"
+   """
