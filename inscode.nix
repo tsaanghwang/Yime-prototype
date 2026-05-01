cf217bdaf377c4e0fbb4fcc54e9be612bf49b64b
音元系统 <tsaang.hwang@outlook.com>
2026-05-01 10:11:44 +0800

    Prune legacy inscode files and shorten README intro

--- inscode.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python310Packages.pip
-    pkgs.python310Full
-  ];
-}
--- pinyin_hanzi.quickquery.sql
+++
@@ -1,6 +0,0 @@
--- database: ./yime/pinyin_hanzi.db
-
-SELECT name, type
-FROM sqlite_master
-WHERE type IN ('table', 'view')
-ORDER BY type, name;
